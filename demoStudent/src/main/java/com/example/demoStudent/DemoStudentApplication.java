862f87f42578a508e619b53ee4622c3bec951ecc
AS20171435 <AS20171435@APL-F8RB403>
2020-12-29 14:12:40 +0530

    .

--- demoStudent/src/main/java/com/example/demoStudent/DemoStudentApplication.java
+++ demoStudent/src/main/java/com/example/demoStudent/DemoStudentApplication.java
@@ -1,13 +1,22 @@
 package com.example.demoStudent;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoStudentApplication {
+public class DemoStudentApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoStudentApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
+	
 }
--- demoStudent/src/test/java/com/example/demoStudent/repo/RepoTest.java
+++ demoStudent/src/test/java/com/example/demoStudent/repo/RepoTest.java
@@ -2,6 +2,7 @@ package com.example.demoStudent.repo;
 
 import org.junit.jupiter.api.Assertions;
 
+
 import org.junit.jupiter.api.Test;
 
 public class RepoTest 
